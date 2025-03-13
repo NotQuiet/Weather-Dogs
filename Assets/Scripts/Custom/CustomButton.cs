49b276b8eb515d673b2ec5b6eb64424b28313d8b
NotQuietGitLab <mikhail.work.flow@gmail.com>
2025-03-13 17:48:02 +0300

    Add unirx & unitask

---
+++ Assets/Scripts/Custom/CustomButton.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Custom
+{
+    public class CustomButton : MonoBehaviour, IPointerUpHandler
+    {
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/MVC/Views/ButtonsView.cs
+++ Assets/Scripts/MVC/Views/ButtonsView.cs
@@ -1,10 +1,12 @@
 using MVC.Abstract;
 using MVC.Models;
+using UnityEngine;
 
 namespace MVC.Views
 {
     public class ButtonsView : View<ButtonsModel, ButtonsView>
     {
+        
 
     }
 }
